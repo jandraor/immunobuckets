d0a66507bb2c8eef7bf8e889b6d80c405d0688fc
Jair Andrade <jair.albert.andrade@gmail.com>
2025-06-26 22:56:26 +0100

    Update titre dynamics

--- R/titre_dynamics.R
+++ R/titre_dynamics.R
@@ -1,15 +1,11 @@
 #' Add titres to infection history
 #'
+#' @param short_options A list of parameters to configure the short-term decay
+#'  rate.
+#' @param long_options A list of parameters to configure the long-term decay
+#'  rate.
+#' @param rise_options A list of parameters to configure the rise.
 #' @param inf_history_df A data frame
-#' @param long_decay_coefficient A scalar in the interval (0, 1)
-#'   that modulates the long-term decay rate, representing the proportion of the
-#'   long-term rate that is effectively applied to the system. A value of 1
-#'   keeps the rate constant after subsequent infection.
-#' @param ltr_0
-#' @param long_term_rate_dynamics
-#' @param peak_scaling_factor
-#' @param initial_rise A numeric value in the log scale.
-#' @param peak_dynamics
 #'
 #' @returns A data frame
 #' @export
@@ -18,31 +14,28 @@
 #' inf_history_df <-
 #'   data.frame(subject_id = 1,
 #'              year_index = 11:23,
+#'              age        = 2:14,
 #'              sim_y      = 0)
-#'
 #' inf_history_df$sim_y[c(6, 8, 11, 12)] <- 1
 #'
-#' add_titre_to_infection_history(inf_history_df)
+#' add_titre_to_infection_history(
+#' inf_history_df,
+#' long_options  = list(L = 0.22, a = -0.96, b = 18.6),
+#' short_options = list(intercept = 0.02,
+#' slope     = 0.003342),
+#' rise_options  = list(intercept = 8,
+#'                      slope     = -0.8))
 add_titre_to_infection_history <- function(
     inf_history_df,
-    ltr_0,
-    long_term_rate_dynamics = "decreasing",
-    long_decay_coefficient  = 1,
-    initial_rise,
-    peak_dynamics,
-    peak_options)
+    short_options,
+    long_options,
+    rise_options)
 {
   inf_history_df$cumulative_infections <- cumsum(inf_history_df$sim_y)
 
   df_list <- split(inf_history_df, inf_history_df$cumulative_infections)
 
   n_df <- length(df_list)
-
-  long_decay <- ltr_0
-
-  log_peak_0 <- initial_rise
-
-  A0 <- exp(log_peak_0) # Rise
 
   baseline_titre <- 0
 
@@ -52,24 +45,20 @@ add_titre_to_infection_history <- function(
     n_inf <- unique(df$cumulative_infection)
     n_row <- nrow(df)
 
-    #Long-decay rate
-    long_rate <- estimate_decay_rate(n_inf, ltr_0, long_term_rate_dynamics,
-                                     long_decay_coefficient)
-
     if(n_inf == 0) log_titre <- rep(0, n_row)
 
     if(n_inf > 0)
     {
-      if(peak_dynamics == "linear_rise")
-      {
-        peak_options$baseline_titre <- baseline_titre
-      }
+      rise     <- estimate_rise(baseline_titre, rise_options)
+      log_peak <- baseline_titre + rise
 
-      log_peak <- estimate_log_peak(
-        log_peak_0   = log_peak_0,
-        n_inf        = n_inf,
-        method       = peak_dynamics,
-        peak_options = peak_options)
+      # short-term decay rate
+      short_rate <- estimate_short_rate(rise, short_options)
+
+      # Long-term decay rate
+      long_rate <- estimate_long_rate(c(df$age[[1]], df$age), long_options)
+
+      long_rate <- long_rate / 360
 
       anchor_index <- min(df$year_index)
 
@@ -81,8 +70,8 @@ add_titre_to_infection_history <- function(
 
       titre        <- bi_exponential_decay(t          = t_indexes,
                                            A0         = exp(log_peak),
-                                           par_sigma  = 1/30,
-                                           par_lambda = long_rate,
+                                           par_sigma  = short_rate,
+                                           par_gamma = long_rate,
                                            par_rho    = 0.98)
       baseline_titre <- log(titre[length(titre)])
 
@@ -114,50 +103,28 @@ add_titre_to_infection_history <- function(
 #'
 #' @examples
 #' bi_exponential_decay(365, exp(10), 0.98, 1/720, 1/30)
-bi_exponential_decay <- function(t, A0, par_sigma, par_lambda, par_rho)
+bi_exponential_decay <- function(t, A0, par_sigma, par_gamma, par_rho)
 {
   A0 * (par_rho * exp(-par_sigma * t) +
-          (1 - par_rho) * exp(-par_lambda * t))
+          (1 - par_rho) * exp(-par_gamma * t))
 }
 
-# @param p A scalar corresponding to the growth exponent for the power law
-# @param a A scalar corresponding to the scaling factor
-# @param peak_options A list
-estimate_log_peak <- function(log_peak_0, n_inf, method, peak_options)
+estimate_short_rate <- function(rise, short_options) {
+
+  short_options$intercept + short_options$slope * rise
+}
+
+estimate_rise <- function(baseline_titre, rise_options) {
+  rise_options$intercept + rise_options$slope * baseline_titre
+}
+
+estimate_long_rate <- function(age, long_options)
 {
-  if(n_inf == 1) return(log_peak_0)
+  L <- long_options$L
+  a <- long_options$a
+  b <- long_options$b
 
-  if(method == "power_law")
-  {
-    a        <- peak_options$a
-    p        <- peak_options$p
-    return(log_peak_0 + a * (n_inf - 1)^p)
-  }
-
-  if(method == "linear_rise")
-  {
-    intercept      <- peak_options$intercept
-    slope          <- peak_options$slope
-    baseline_titre <- peak_options$baseline_titre
-
-    linear_rise <-  intercept + slope * baseline_titre
-
-    return(baseline_titre + linear_rise)
-  }
-
-  stop()
+  L / (1 + exp(-a * (age - b)))
 }
 
-estimate_decay_rate <- function(n_inf, ltr_0, long_term_rate_dynamics,
-                                long_decay_coefficient) {
 
-  if(long_term_rate_dynamics == "decreasing")
-  {
-    return(ltr_0 * long_decay_coefficient ** (n_inf - 1))
-  }
-
-  if(long_term_rate_dynamics == "piecewise")
-  {
-    return(ifelse(n_inf > 4, 0, ltr_0))
-  }
-}
--- tests/testthat/test-titre_dynamics.R
+++ tests/testthat/test-titre_dynamics.R
@@ -3,182 +3,100 @@ test_that("add_titre_to_infection_history() works",
   inf_history_df <-
     data.frame(subject_id = 1,
                year_index = 11:23,
+               age        = 2:14,
                sim_y      = 0)
 
   inf_history_df$sim_y[c(6, 8, 11, 12)] <- 1
 
-  actual <- add_titre_to_infection_history(inf_history_df,
-                                           long_decay_coefficient = 1,
-                                           initial_rise  = 10,
-                                           peak_dynamics = "power_law",
-                                           peak_options  = list(p = 0.5, a = 0),
-                                           ltr_0                  = 1/720)
-
-  expected <- inf_history_df
-
-  titre_after_one_year <- bi_exponential_decay(360, exp(10),
-                                               1/30, 1/720, par_rho = 0.98) |>
-    log()
-
-  titre_after_two_years <- bi_exponential_decay(720, exp(10),
-                                                1/30, 1/720, par_rho = 0.98) |>
-    log()
-
-  expected$cumulative_infections <- cumsum(expected$sim_y)
-
-  expected$log_titre <- c(rep(0, 5), 10,
-                          titre_after_one_year, 10,  titre_after_one_year,
-                          titre_after_two_years , 10, 10,
-                          titre_after_one_year)
-
-  expect_equal(actual, expected)
-})
-
-test_that("add_titre_to_infection_history() works with a piecewise rate",
-{
-  inf_history_df <-
-    data.frame(subject_id = 1,
-               year_index = 11:23,
-               sim_y      = 0)
-
-  inf_history_df$sim_y[c(3, 5, 7, 8, 10)] <- 1
-
   actual <- add_titre_to_infection_history(
     inf_history_df,
-    ltr_0                   = 1/720,
-    long_term_rate_dynamics = "piecewise",
-    initial_rise            = 10,
-    peak_dynamics           = "power_law",
-    peak_options            = list(p = 0.5, a = 0))
+    long_options  = list(L = 0.22, a = -0.96, b = 18.6),
+    short_options = list(intercept = 0.02,
+                         slope     = 0.003342),
+    rise_options  = list(intercept = 8,
+                         slope     = -0.8))
 
   expected <- inf_history_df
 
-  first_inf <- bi_exponential_decay(c(0, 360), exp(10),
-                                    1/30, 1/720, par_rho = 0.98) |> log()
+  first_inf <- bi_exponential_decay(
+    c(0, 360, 720), exp(8),
+    par_sigma = 0.02 + 0.003342*8,
+    par_gamma = (0.22 / (1 + exp(0.96 * (c(7, 7:8) - 18.6)))) / 360,
+    par_rho = 0.98) |> log()
 
-  second_inf <- first_inf
+  rise <- 8 + first_inf[3] * -0.8
 
-  third_inf <- 10
+  second_inf <- bi_exponential_decay(
+    360 * 0:3,
+    exp(first_inf[3] + rise),
+    0.02 + 0.003342 * rise,
+    par_gamma = (0.22 / (1 + exp(0.96 * (c(9, 9:11) - 18.6)))) / 360,
+    par_rho = 0.98) |> log()
 
-  fourth_infection <- first_inf
+  rise <- 8 + second_inf[4] * -0.8
 
-  fifth_infection <- bi_exponential_decay(
-    t          = 360 * 0:3, exp(10),
-    par_sigma  = 1/30,
-    par_lambda =  0, par_rho = 0.98) |> log()
+  third_inf <- bi_exponential_decay(
+    360 * 0:1,
+    A0 = exp(second_inf[4] + rise),
+    0.02 + 0.003342 * rise,
+    par_gamma = (0.22 / (1 + exp(0.96 * (c(12, 12) - 18.6)))) / 360,
+    par_rho = 0.98) |>
+    log()
 
-  expected$cumulative_infections <- cumsum(expected$sim_y)
+  rise <- 8 + third_inf[2] * -0.8
 
-  expected$log_titre <- c(rep(0, 2), first_inf, second_inf, third_inf,
-                          fourth_infection, fifth_infection)
-
-  expect_equal(actual, expected)
-})
-
-test_that("add_titre_to_infection_history() works with a linear rises",
-{
-  inf_history_df <-
-    data.frame(subject_id = 1,
-               year_index = 11:23,
-               sim_y      = 0)
-
-  inf_history_df$sim_y[c(3, 8, 12)] <- 1
-
-  actual <- add_titre_to_infection_history(
-    inf_history_df = inf_history_df,
-    long_decay_coefficient = 1,
-    initial_rise  = 8,
-    peak_dynamics = "linear_rise",
-    peak_options  = list(slope     = -0.8,
-                         intercept = 9.95),
-    ltr_0                  = 1/720)
-
-  expected <- inf_history_df
-
-  first_inf <- bi_exponential_decay(360 * 0:5, exp(8),
-                                    1/30, 1/720, par_rho = 0.98) |> log()
-
-  last_titre <- first_inf[length(first_inf)]
-
-  log_new_peak <- last_titre + 9.95 - 0.8 * last_titre
-
-  second_inf <- bi_exponential_decay(360 * 0:4, exp(log_new_peak),
-                                    1/30, 1/720, par_rho = 0.98) |> log()
-
-  last_titre <- second_inf [length(second_inf)]
-
-  log_new_peak <- last_titre + 9.95 - 0.8 * last_titre
-
-  third_inf  <- bi_exponential_decay(360 * 0:2, exp(log_new_peak),
-                                     1/30, 1/720, par_rho = 0.98) |> log()
+  fourth_inf <- bi_exponential_decay(
+    360 * 0:1,
+    A0 = exp(third_inf[2] + rise),
+    0.02 + 0.003342 * rise,
+    par_gamma = (0.22 / (1 + exp(0.96 * (c(13, 13) - 18.6)))) / 360,
+    par_rho = 0.98) |>
+    log()
 
   expected$cumulative_infections <- cumsum(expected$sim_y)
 
-  expected$log_titre <- c(rep(0, 2),
-                          first_inf[-length(first_inf)],
-                          second_inf[-length(second_inf)],
-                          third_inf[-length(third_inf)])
+  expected$log_titre <- c(rep(0, 5), first_inf[1:2],
+                          second_inf[1:3],
+                          third_inf[1],
+                          fourth_inf)
 
   expect_equal(actual, expected)
 })
 
-test_that("estimate_log_peak() works with the power law option", {
-
-  peak_options <- list(a = 2, p = 0.5)
-
-  actual   <- estimate_log_peak(log_peak_0 = 10, n_inf = 1,
-                                method = "power_law",
-                                peak_options = peak_options)
-
-
-  expected <- 10
-
-  expect_equal(actual, expected)
-
-  actual   <- estimate_log_peak(log_peak_0 = 10, n_inf = 2,
-                                method = "power_law",
-                                peak_options = peak_options)
-  expected <- 12
-
-  expect_equal(actual, expected)
-})
-
-test_that("estimate_log_peak() works with the linear rise options",
+test_that("estimate_long_rate() works",
 {
-  peak_options <- list(
-    baseline_titre = 3,
-    slope          = -0.8,
-    intercept      = 9.95)
+  long_options <- list(L = 0.22, a = -0.96, b = 18.6)
 
-  actual   <- estimate_log_peak(log_peak_0 = 8, n_inf = 1,
-                                method = "linear_rise",
-                                peak_options)
+  age <- 7:9
+
+  actual <- estimate_long_rate(age = 7:9, long_options)
+
+  expected <- 0.22 / (1 + exp(0.96 * (7:9 - 18.6)))
+
+  expect_equal(actual, expected)
+})
+
+test_that("estimate_rise() works",
+{
+  rise_options <- list(intercept = 8,
+                       slope     = 0.8)
+
+  actual <- estimate_rise(baseline_titre = 0, rise_options)
+
   expected <- 8
 
   expect_equal(actual, expected)
-
-  peak_options <- list(
-    baseline_titre = 3,
-    slope          = -0.8,
-    intercept      = 9.95)
-
-  actual   <- estimate_log_peak(log_peak_0 = 8, n_inf = 2,
-                                method = "linear_rise",
-                                peak_options)
-  expected <- 3  + 9.95 - 0.8 * 3
-
-  expect_equal(actual, expected)
 })
 
-test_that("estimate_decay_rate works()",
-{
-  actual <- estimate_decay_rate(4, 1/720, "piecewise", 1)
+test_that("estimate_short_rate() works", {
 
-  expected <- 1/720
+  short_options <- list(intercept = 0.02,
+                        slope     = 0.003342)
+
+  actual <- estimate_short_rate(rise = 0, short_options)
+
+
+  expected <- 0.02
 
   expect_equal(actual, expected)
-
-  actual <- estimate_decay_rate(5, 0, "piecewise", 1)
-
-  expected <- 1/720
 })
